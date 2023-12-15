0861c37482f423c831ccc02f248b2717da8d8c5f
YE <youssef.ejjiyar-etu@etu.univh2c.ma>
2023-12-15 22:48:43 +0100

    adding explore spots files

---
+++ src/components/Traitcolor.js
@@ -0,0 +1,13 @@
+import "../assets/css/foods.css";
+
+function Traitcolor(){
+    return(
+        <div style={{height:"80px",backgroundColor:"#CDD1D4",display:"flex",justifyContent:"center",alignItems:"center"}} className="respotrait">
+            <hr style={{width:"80%",borderBottom: "1px solid #21434C",borderTop:"none"}}/>
+        </div>
+    );
+}
+
+
+
+export default Traitcolor;
--- src/pages/ExploreSpots.js
+++ src/pages/ExploreSpots.js
@@ -1,11 +1,19 @@
+import Foods from "../components/Foods";
 import Header from "../components/Header";
-
-
+import Places from "../components/Places";
+import Traitcolor from "../components/Traitcolor";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
 
 function ExploreSpots(){
     return(
         <div>
             <Header/>
+            <Places/>
+            <Traitcolor/>
+            <Foods/>
+            <Contact/>
+            <Footer/>
         </div>
     );
 }
