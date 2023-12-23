4fa7a23ab50781c1c317180eed8fcadfd8b68f5d
Jaraf Salma <115297113+salmajaraf@users.noreply.github.com>
2023-12-23 12:42:29 +0100

    Commit remaining changes after merge

--- src/components/Traitcolor.js
+++ src/components/Traitcolor.js
@@ -1,4 +1,4 @@
-//import "../assets/css/foods.css";
+import "../assets/css/foods.css";
 
 function Traitcolor(){
     return(
@@ -7,7 +7,5 @@ function Traitcolor(){
         </div>
     );
 }
-
-
 
 export default Traitcolor;
