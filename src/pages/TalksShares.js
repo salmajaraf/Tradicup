291f33cac10add7e477752cab6eb7bed3aa936b5
Jaraf Salma <115297113+salmajaraf@users.noreply.github.com>
2023-12-27 16:17:03 +0100

    add talks and share but i will continue in another branch (#4)

--- src/pages/TalksShares.js
+++ src/pages/TalksShares.js
@@ -1,11 +1,17 @@
 import Header from "../components/Header";
-
-
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+import Talks from "../components/Talks";
+import Countries from "../components/Countries";
 
 function TalksShares(){
     return(
         <div>
             <Header/>
+            <Talks/>
+            <Countries/>
+            <Contact/>
+            <Footer/>
         </div>
     );
 }
